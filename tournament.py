a719de72b4062eed019a16bef3020f8a8b552bfd
slv-improver <k.moret.slv@gmail.com>
2022-06-02 22:04:36 +0100

    add tournament model

---
+++ tournament.py
@@ -0,0 +1,26 @@
+class Tournament:
+    """The tournament will manage matches between players"""
+
+    def __init__ (
+        self,
+        /,
+        name,
+        place,
+        date,
+        *,
+        end_date=None,
+        number_of_rounds=4,
+        round_list=[],
+        player_list=[],
+        time_management,
+        description="Pas de remarques"
+    ):
+        self.name = name
+        self.place = place
+        self.date = date
+        self.end_date = end_date
+        self.number_of_rounds = number_of_rounds
+        self.round_list = round_list
+        self.player_list = player
+        self.time_management = time_management
+        self.description = description
