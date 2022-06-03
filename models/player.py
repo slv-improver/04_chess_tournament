2894dfeda282ada90821f397cd889e009a099e7c
slv-improver <k.moret.slv@gmail.com>
2022-06-03 16:42:24 +0100

    add player_id

--- models/player.py
+++ models/player.py
@@ -7,10 +7,12 @@ class Player:
         first_name,
         birth_date,
         gender,
-        ranking
+        ranking,
+        player_id=None
     ):
         self.last_name = last_name
         self.first_name = first_name
         self.birth_date = birth_date
         self.gender = gender
         self.ranking = ranking
+        self.player_id = player_id
