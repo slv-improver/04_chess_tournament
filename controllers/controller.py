461a6fbb328360648496b8e79cad70c26438c670
slv-improver <k.moret.slv@gmail.com>
2022-06-10 15:57:26 +0100

    display the menu

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,11 +1,17 @@
 from views.welcome import Welcome
+from views.menu import Menu
 
 class Controller:
     """Base controller"""
 
     def __init__(self):
         self.welcome()
+        self.menuLister()
 
     def welcome(self):
         welcome = Welcome()
         print(welcome.display)
+
+    def menuLister(self):
+        menu = Menu()
+        print(menu.display)
---
+++ views/menu.py
@@ -0,0 +1,10 @@
+class Menu:
+    """Display the menu"""
+
+    def __init__(self):
+        self.display = (
+            '1— Créer un tournoi\n'
+            '2— Ajouter un joueur\n'
+            '3— Générer un rapport\n'
+            '4— Quitter\n'
+        )
