721ffba802a557844ee4e6c9b7162d7ba5583ba1
slv-improver <k.moret.slv@gmail.com>
2022-06-10 15:24:13 +0100

    display the Welcome message

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,5 +1,11 @@
+from views.welcome import Welcome
+
 class Controller:
     """Base controller"""
 
     def __init__(self):
-        pass
+        self.welcome()
+
+    def welcome(self):
+        welcome = Welcome()
+        print(welcome.display)
---
+++ views/welcome.py
@@ -0,0 +1,11 @@
+class Welcome:
+    """Display the Welcome message"""
+
+    def __init__(self):
+        self.display = """
+        ————————————————————
+        |  Welcome to the  |
+        | Chess Tournament |
+        |      program     |
+        ————————————————————
+        """
