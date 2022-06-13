7450e34a61e03fe11105a2288c43fe5a0fa8d411
slv-improver <k.moret.slv@gmail.com>
2022-06-13 23:11:08 +0100

    ask for player info

--- controllers/playerController.py
+++ controllers/playerController.py
@@ -4,4 +4,13 @@ class PlayerController:
     """Manage Player creation"""
 
     def __init__(self):
-        pass
+        self.playerView = PlayerView()
+        print(self.playerView.display)
+        self.askPlayerInfo()
+
+    def askPlayerInfo(self):
+        last_name = input('Nom du joueur : ')
+        first_name = input('Prénom : ')
+        birth_date = input('Date de naissance : ')
+        gender = input('M / F')
+        ranking = input('Rang : ')
---
+++ views/player.py
@@ -0,0 +1,6 @@
+class Player:
+
+    def __init__(self):
+        self.display = (
+            '\n—— Création de joueur ——\n'
+        )
