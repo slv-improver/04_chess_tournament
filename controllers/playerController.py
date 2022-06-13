8b29efa59b0591cfdd1fdc3cbb39c30c71b781f6
slv-improver <k.moret.slv@gmail.com>
2022-06-13 23:22:56 +0100

    instantiate PlayerModel in PlayerController

--- controllers/playerController.py
+++ controllers/playerController.py
@@ -1,4 +1,5 @@
 from views.player import Player as PlayerView
+from models.player import Player as PlayerModel
 
 class PlayerController:
     """Manage Player creation"""
@@ -6,11 +7,20 @@ class PlayerController:
     def __init__(self):
         self.playerView = PlayerView()
         print(self.playerView.display)
+        self.PlayerModel = None
         self.askPlayerInfo()
 
     def askPlayerInfo(self):
         last_name = input('Nom du joueur : ')
         first_name = input('Prénom : ')
         birth_date = input('Date de naissance : ')
-        gender = input('M / F')
+        gender = input('M / F : ')
         ranking = input('Rang : ')
+
+        self.PlayerModel = PlayerModel(
+            last_name,
+            first_name,
+            birth_date,
+            gender = gender,
+            ranking = ranking
+        )
