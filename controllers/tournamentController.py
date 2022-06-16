7ace26984ecb6a3873e79f7ce63b19cf2d71dda3
slv-improver <k.moret.slv@gmail.com>
2022-06-16 23:11:50 +0100

    offer 3 defined choices for time_management instead of free input

--- controllers/tournamentController.py
+++ controllers/tournamentController.py
@@ -23,9 +23,16 @@ class TournamentController:
         number_of_rounds = input('Le nombre de tours ? (4)')
         if number_of_rounds == '':
             number_of_rounds = 4
-        time_management = input(
-            'Le mode de jeu ?\n Bullet - Blitz - Coup rapide : '
-        )
+        time_management = int(input(
+            'Le mode de jeu ?\n 1—Bullet - 2—Blitz - 3—Coup rapide : '
+        ))
+        match time_management:
+            case 1:
+                time_management = 'Bullet'
+            case 2:
+                time_management = 'Blitz'
+            case 3:
+                time_management = 'Coup rapide'
         description = input('Remarques relatives au tournoi : ')
         if description == '':
             description = "Pas de remarques"
