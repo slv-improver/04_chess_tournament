053e174c00a547ebc7b05b43922b5a5134082a7d
slv-improver <k.moret.slv@gmail.com>
2022-06-17 16:31:07 +0100

    change models.round.py to be able to instantiate it

--- models/round.py
+++ models/round.py
@@ -4,8 +4,8 @@ from datetime import datetime
 class Round:
     """List of matches"""
 
-    def __init__(self, name, matches_list):
+    def __init__(self, name):
         self.name = name
-        self.matches_list = matches_list
+        self.matches_list = []
         self.start_time = datetime.today()
-        self.end_time = datetime.today()
+        self.end_time = None
