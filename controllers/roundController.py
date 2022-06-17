9b4d588a40f43de25a4bc577743b69a795b544dc
slv-improver <k.moret.slv@gmail.com>
2022-06-17 16:11:07 +0100

    make round_number property

--- controllers/roundController.py
+++ controllers/roundController.py
@@ -4,13 +4,16 @@ from models.round import Round as RoundModel
 class RoundController:
 
     def __init__(self, round_number):
-        self.roundView = RoundView(round_number)
+        self.round_number = round_number
+        self.roundView = RoundView(self.round_number)
         print(self.roundView.display)
         self.roundModel = None
         self.askRoundInfo()
 
     def askRoundInfo(self):
-        pass
+        name = input(f'Le nom du tour : (Round {self.round_number}) ')
+        if name = '':
+            name = f'Round{self.round_number}'
 
 # generate pairs of players
 # ask for results
