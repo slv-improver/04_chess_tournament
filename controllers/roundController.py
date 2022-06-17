04a651ad440ac32cfa412fa0a27a3fd30ee82202
slv-improver <k.moret.slv@gmail.com>
2022-06-17 16:05:02 +0100

    import roundModel

--- controllers/roundController.py
+++ controllers/roundController.py
@@ -1,10 +1,12 @@
 from views.round import Round as RoundView
+from models.round import Round as RoundModel
 
 class RoundController:
 
     def __init__(self, round_number):
         self.roundView = RoundView(round_number)
         print(self.roundView.display)
+        self.roundModel = None
 
 # generate pairs of players
 # ask for results
