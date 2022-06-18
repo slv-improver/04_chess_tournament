7ea17fd8d3845b3d14330e3365a98769c4dc1b13
slv-improver <k.moret.slv@gmail.com>
2022-06-18 19:51:45 +0100

    add matchView

--- controllers/matchController.py
+++ controllers/matchController.py
@@ -1,5 +1,8 @@
+from views.match import Match as MatchView
+
 class MatchController:
 
     def __init__(self, player1, player2):
         self.player1 = player1
         self.player2 = player2
+        self.matchView = MatchView(player1, player2)
---
+++ views/match.py
@@ -0,0 +1,6 @@
+class Match:
+
+    def __init__(self, player1, player2):
+        self.display = (
+            f'\n———— Match opposant {player1} à {player2} ————\n'
+        )
