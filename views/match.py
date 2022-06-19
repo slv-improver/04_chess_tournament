a30f86d018aa13cced4f08ada62700c5d148a5cf
slv-improver <k.moret.slv@gmail.com>
2022-06-19 14:36:39 +0100

    optimize MatchView.display

--- views/match.py
+++ views/match.py
@@ -2,7 +2,8 @@ class Match:
 
     def __init__(self, player1, player2):
         self.display = (
-            f'\n———— Match opposant : \n\
+            f'\n———— Match : \n\
             {player1.last_name} {player1.first_name} \n\
-            à {player2.last_name} {player2.first_name} ————\n'
+            contre \n\
+            {player2.last_name} {player2.first_name} ————\n'
         )
