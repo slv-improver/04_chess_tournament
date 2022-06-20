b2cdf6902d0853f6b352c0e74f5b841cbf18c717
slv-improver <k.moret.slv@gmail.com>
2022-06-20 15:37:17 +0100

    change loop condition

--- controllers/roundController.py
+++ controllers/roundController.py
@@ -42,11 +42,11 @@ class RoundController:
             key=lambda x: (-x.tournamentPoints, x.ranking)
         )
 
-        for i in range(len(ordered_player_list)):
-            if (i+1) % 2 != 0:
-                MatchController(
-                    ordered_player_list[i], ordered_player_list[i+1]
-                )
+        while len(ordered_player_list) > 0:
+            MatchController(
+                ordered_player_list.pop(i),
+                ordered_player_list.pop(i+1)
+            )
 
     def askMatchResult(self):
         for match in self.matches_list:
