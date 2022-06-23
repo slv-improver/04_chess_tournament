3b0e402ff3dd0f98ab0ef48c384c579631cb8d6a
slv-improver <k.moret.slv@gmail.com>
2022-06-23 19:32:08 +0100

    add clearTable() to remove data

--- dao/playerDao.py
+++ dao/playerDao.py
@@ -3,3 +3,6 @@ class PlayerDAO(DAO):
     def __init(self):
         super().__init()
         self.players_table = self.db.table('players')
+
+    def clearTable(self):
+        self.players_table.truncate()
