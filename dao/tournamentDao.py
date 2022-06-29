b1ac4173c05e98ccf1265acae783704fc11e4870
slv-improver <k.moret.slv@gmail.com>
2022-06-29 23:46:09 +0100

    implement the same methods as in PlayerDAO

--- dao/tournamentDao.py
+++ dao/tournamentDao.py
@@ -6,3 +6,13 @@ class TournamentDAO(DAO):
     def __init__(self):
         super().__init__()
         self.tournaments_table = self.db.table('tournament')
+
+    def getAll(self):
+        return self.tournaments_table.all()
+
+    def clearTable(self):
+        self.tournaments_table.truncate()
+
+    def insertData(self, serializedData):
+        self.clearTable()
+        self.tournaments_table.insert_multiple(serializedData)
