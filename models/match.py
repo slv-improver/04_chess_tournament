e8312064eb366d4f163fb48952d1dfe36696febb
slv-improver <k.moret.slv@gmail.com>
2022-07-03 00:16:33 +0100

    add comment to explain scores in Match model

--- models/match.py
+++ models/match.py
@@ -5,7 +5,7 @@ class Match(Base):
     """Each match played by the players"""
 
     def __init__(self, scores):
-        self.scores = scores
+        self.scores = scores # ([player1, 0], [player2, 0])
 
     def updateScores(self, score1, score2):
         self.scores[0][1] = score1
