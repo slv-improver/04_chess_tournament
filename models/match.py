ca7dfc1b8fb7bf8f5e435eb0af867e1dbcfad0c8
slv-improver <k.moret.slv@gmail.com>
2022-07-11 23:10:20 +0100

    overwrite toDict() for models.Match

--- models/match.py
+++ models/match.py
@@ -10,3 +10,14 @@ class Match(Base):
     def updateScores(self, score1, score2):
         self.scores[0][1] = score1
         self.scores[1][1] = score2
+
+    def toDict(self):
+        p1 = self.scores[0]
+        p2 = self.scores[1]
+        dictData = {
+            'scores': (
+                [f'{p1[0].last_name} {p1[0].first_name}', p1[1]],
+                [f'{p2[0].last_name} {p2[0].first_name}', p2[1]]
+            )
+        }
+        return dictData
