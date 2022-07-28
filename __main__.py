feddee4c027ade4fd2db70fe6bc4e0c732783324
slv-improver <k.moret.slv@gmail.com>
2022-07-28 17:07:11 +0100

    implement saveGame() & loadGame

--- __main__.py
+++ __main__.py
@@ -6,6 +6,12 @@ from controllers.controller import Controller
 
 game_controller = Controller()
 
+def saveGame():
+    game_controller.saveGame()
+
+def loadGame():
+    game_controller.loadGame()
+
 def main():
     game_controller.launchGame()
 
