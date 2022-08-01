b0a6590e960dd964059e28d2c7c5c83e0ba83957
slv-improver <k.moret.slv@gmail.com>
2022-08-02 00:09:05 +0100

    change name of db table 'tournament' => 'tournaments'

--- dao/tournamentDao.py
+++ dao/tournamentDao.py
@@ -5,7 +5,7 @@ class TournamentDAO(DAO):
 
     def __init__(self):
         super().__init__()
-        self.tournaments_table = self.db.table('tournament')
+        self.tournaments_table = self.db.table('tournaments')
 
     def getAll(self):
         return self.tournaments_table.all()
