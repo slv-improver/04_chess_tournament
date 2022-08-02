deabcbe638f2be52961582a22381510411b9c7e1
slv-improver <k.moret.slv@gmail.com>
2022-08-02 16:08:25 +0100

    remove clearTable() for TournamentDao

--- dao/tournamentDao.py
+++ dao/tournamentDao.py
@@ -10,8 +10,5 @@ class TournamentDAO(DAO):
     def getAll(self):
         return self.tournaments_table.all()
 
-    def clearTable(self):
-        self.tournaments_table.truncate()
-
     def insertData(self, serializedData):
         self.tournaments_table.insert(serializedData)
